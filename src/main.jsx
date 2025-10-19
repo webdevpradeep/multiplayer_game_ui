8e2950e8ecf14fb93a5def4ee5a4446b358fbad5
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-10-19 12:15:28 +0530

    add : add profile route to App component for user profile access

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router';
+import { GlobalContextProvider } from './context/globalContext.jsx';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GlobalContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GlobalContextProvider>
   </StrictMode>
 );
