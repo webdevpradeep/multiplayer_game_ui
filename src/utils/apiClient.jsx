cd8c4a4d5ab37f1ab8947dea33eb6b3c524cb79d
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-10-27 19:32:43 +0530

    fix: update access token retrieval and ensure token is used for game-…

--- src/utils/apiClient.jsx
+++ src/utils/apiClient.jsx
@@ -1,10 +1,9 @@
 import { getCookie } from './cookies';
 
-// const baseURL = "http://192.168.1.150:5000";
 const baseURL = 'http://localhost:5000';
 
 export const apiClient = async (url, method, body = null, tokenName = '') => {
-  const accessToken = getCookie('access_token');
+  const accessToken = getCookie('token');
 
   const defaultHeaders = {
     'Content-Type': 'application/json',
@@ -41,19 +40,25 @@ export const apiClient = async (url, method, body = null, tokenName = '') => {
 
 // user authentication ...............................
 // Auth APIs
-apiClient.login = async (payload) =>
-  await apiClient('/users/login', 'POST', payload);
+apiClient.login = (payload) => apiClient('/users/login', 'POST', payload);
 
-apiClient.signup = async (payload) =>
-  await apiClient('/users/signup', 'POST', payload);
+apiClient.signup = (payload) => apiClient('/users/signup', 'POST', payload);
 
-apiClient.resetPassword = async (token, payload) =>
-  await apiClient(`/users/resetPassword/${token}`, 'PATCH', payload);
+apiClient.resetPassword = (token, payload) =>
+  apiClient(`/users/resetPassword/${token}`, 'PATCH', payload);
 
-apiClient.forgotPassword = async (payload) =>
-  await apiClient('/users/forgotPassword', 'PATCH', payload);
+apiClient.forgotPassword = (payload) =>
+  apiClient('/users/forgotPassword', 'PATCH', payload);
 
 apiClient.uploadProfileImage = (payload) =>
   apiClient('/users/profile/image', 'PATCH', payload);
 
-apiClient.listGames = async () => await apiClient('/games', 'GET');
+apiClient.addGame = (payload) => apiClient('/games', 'POST', payload, 'token');
+
+apiClient.requestGame = (payload) =>
+  apiClient('/games/request', 'POST', payload, 'token');
+
+// apiClient.getGames = () => apiClient("/games", "GET");
+
+apiClient.googleLogin = (payload) =>
+  apiClient('/users/login/google', 'POST', payload);
